b3832df98363e39aeef051cab68563ed8ff62b0f
loco9939 <kls9939@naver.com>
2023-02-18 21:18:53 +0900

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>Hi welcom to my cocktail intro site! Enjoy yourself!</p>
+    </section>
+  );
+};
+
+export default About;
